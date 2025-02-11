f855938bfa0a43df876151f22fecee661092fbc7
lukas884 <157029390+lukas884@users.noreply.github.com>
2025-02-11 16:54:16 +0200

    fixed issues with old react syntax leading to issues

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-const app = () => {
+const App = () => {
   return (
     <div>
         <h1>GPT-3</h1>
-        <h1>helloooooo</h1>
     </div>
   )
 }
 
-export default app
+export default App
