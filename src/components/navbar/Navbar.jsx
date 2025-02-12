f4c310c993b648356be995193c6e7b4c152f6e5d
lukas884 <157029390+lukas884@users.noreply.github.com>
2025-02-12 17:36:45 +0200

    started styling the header, added all folders

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './navbar.css';
+import {RiMenu3Line, RiCloseLin} from 'react-icons/ri';
+import logo from '../../assets/GPT-3.svg'
+
+const Navbar = () => {
+  return (
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className='gpt3__navbar-links_container'>
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#possibility">OpenAI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
+      <div className='gpt3__navbar-sign'>
+        <p>Sign In</p>
+        <button type='button'> Sign Up</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
