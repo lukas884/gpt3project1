99df136e51eb08f256890b199d190b2ded86d914
lukas884 <157029390+lukas884@users.noreply.github.com>
2025-02-18 10:05:58 +0200

    added articles

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -7,7 +7,7 @@ const Article = ({imgUrl, date, title}) => {
       <div className='gpt3__blog-container_article-image'>
         <img src={imgUrl} alt="blog image" />
       </div>
-      <div className='gpt3__blog-container_article'>
+      <div className='gpt3__blog-container_article-content'>
         <div>
           <p>{date}</p>
           <h3>{title}</h3>
